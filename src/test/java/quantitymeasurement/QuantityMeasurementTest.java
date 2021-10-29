b867c04962e744d5ca025855655e87f9ab0d54a7
SRC0747 <RjSrc@0747>
2021-10-29 14:29:18 +0530

    0 Feet and Null Should return not equal and proves the nulity.

--- src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -18,4 +18,12 @@ class QuantityMeasurementTest {
         Feet feet2 = new Feet(1.0);
         Assertions.assertNotEquals(feet1, feet2);
     }
+
+    @Test
+    public void given0FeetAndNull_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = null;
+        Assertions.assertNotEquals(feet1, feet2);
+    }
+
 }
