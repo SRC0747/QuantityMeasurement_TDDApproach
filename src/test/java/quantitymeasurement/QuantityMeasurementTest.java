3ee42ca8d3833483523b2ccb0c2976236ae82a4d
SRC0747 <RjSrc@0747>
2021-10-29 14:34:21 +0530

    0 Feet and 0 Feet from Reference Checking should return Not Same Reference.

--- src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -26,4 +26,10 @@ class QuantityMeasurementTest {
         Assertions.assertNotEquals(feet1, feet2);
     }
 
+    @Test
+    public void given0FeetAnd0FeetFromDiffRef_ShouldReturnNotSameRef() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assertions.assertNotSame(feet1, feet2);
+    }
 }
