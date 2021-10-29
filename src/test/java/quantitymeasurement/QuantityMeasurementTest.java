81998c554fb1185786ea9b8c51df4ebc36102f16
SRC0747 <RjSrc@0747>
2021-10-30 01:34:59 +0530

    0 Yard and 1 Yard should return true and shows the Non-equality.

--- src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -114,4 +114,11 @@ class QuantityMeasurementTest {
         Length yard2 = new Length(Length.Unit.YARD, 0.0);
         Assertions.assertEquals(yard1, yard2);
     }
+
+    @Test
+    public void given0YardAnd0Yard_ShouldReturnNotEqual() {
+        Length yard1 = new Length(Length.Unit.YARD, 0.0);
+        Length yard2 = new Length(Length.Unit.YARD, 1.0);
+        Assertions.assertNotEquals(yard1, yard2);
+    }
 }
